c78c501e12338f670cae0c818fc218a00a5d3261
charlie <gn4rtistic@gmail.com>
2022-12-07 10:19:14 -0600

    added brand statement to homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,18 +1,36 @@
 import './index.scss'
 import PhotoCarousel from '../PhotoCarousel';
 import Loader from 'react-loaders';
+import { useState, useEffect } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () =>
 {
+  const [ letterClass, setLetterClass ] = useState( 'text-animate' );
+
+  useEffect( () =>
+  {
+    setTimeout( () =>
+    {
+      return setLetterClass( 'text-animate-hover' )
+    }, 3000 )
+  }, [] );
 
   return (
     <>
       <div className='home-page container'>
+        <div className='brand-statement'>
+          <h1><AnimatedLetters
+            letterClass={letterClass}
+            strArray={[ 'M', 'a', 'd', 'e', '', 'I', 'n', '', 'L', 'o', 'v', 'e' ]}
+            idx={15} />
+          </h1>
+        </div>
         <div className='photo-carousel'>
           <PhotoCarousel />
         </div>
       </div>
-            <Loader type="ball-pulse-sync" />
+      <Loader type="ball-pulse-sync" />
     </>
   )
 }
